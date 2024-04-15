0ed58881f3615205c620f8a1049f5a9df5d9aaac
iosdevSW <iosdev.sw@gmail.com>
2024-04-15 11:13:07 +0900

    Initial Commit

---
+++ WidgetApp_Example/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WidgetApp_Example
+//
+//  Created by iOS신상우 on 4/15/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WidgetApp_Example/WidgetApp_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetApp_ExampleApp.swift
+//  WidgetApp_Example
+//
+//  Created by iOS신상우 on 4/15/24.
+//
+
+import SwiftUI
+
+@main
+struct WidgetApp_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
